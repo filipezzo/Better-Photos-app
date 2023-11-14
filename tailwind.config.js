77d733a93d9da6494f882b6ac5ea0bcdcfe4bf28
Filipe Avanzzo <avanzzodev@gmail.com>
2023-11-14 15:03:47 -0300

    finish static page and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,28 @@ export default {
       gridTemplateRows: {
         "grid-area": "65px 600px 1fr",
       },
+
+      animation: {
+        animate: "animate 1s backwards",
+        animateDelay1: "animate  1s 0.1s backwards",
+        animateDelay2: "animate 1s 0.2s  backwards",
+        animateDelay3: "animate 1s  0.3s backwards",
+        animateDelay4: "animate 1s 0.4s  backwards",
+        animateDelay5: "animate 1s 0.5s  backwards",
+      },
+      keyframes: {
+        animate: {
+          "0%": {
+            opacity: 0,
+
+            transform: "scale(1.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "scale(1)",
+          },
+        },
+      },
     },
   },
   plugins: [],
